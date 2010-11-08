c2b62f3e49e69178b7e530f7d0b8299230c8e5e1
Pete Yandell <pete@notahat.com>
2010-11-08 11:04:19 +1100

    Added some actual, you know, code and stuff.

--- spec/safe_shell_spec.rb
+++ spec/safe_shell_spec.rb
@@ -1,7 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SafeShell" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  it "should return the output of the command" do
+    SafeShell.execute("echo", "Hello, world!").should == "Hello, world!\n"
   end
+
+  it "should set $? to the exit status of the command" do
+    SafeShell.execute("test", "a", "=", "a")
+    $?.exitstatus.should == 0
+
+    SafeShell.execute("test", "a", "=", "b")
+    $?.exitstatus.should == 1
+  end
+
+  it "should safely handle dangerous characters in command arguments" do
+    SafeShell.execute("echo", ";date").should == ";date\n"
+  end
+
 end
