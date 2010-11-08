298111ac14e20bd4751564c76f0ee6f451ee5d65
Pete Yandell <pete@notahat.com>
2010-11-08 15:40:53 +1100

    More specs.

--- spec/safe_shell_spec.rb
+++ spec/safe_shell_spec.rb
@@ -6,6 +6,10 @@ describe "SafeShell" do
     SafeShell.execute("echo", "Hello, world!").should == "Hello, world!\n"
   end
 
+  it "should safely handle dangerous characters in command arguments" do
+    SafeShell.execute("echo", ";date").should == ";date\n"
+  end
+
   it "should set $? to the exit status of the command" do
     SafeShell.execute("test", "a", "=", "a")
     $?.exitstatus.should == 0
@@ -14,8 +18,22 @@ describe "SafeShell" do
     $?.exitstatus.should == 1
   end
 
-  it "should safely handle dangerous characters in command arguments" do
-    SafeShell.execute("echo", ";date").should == ";date\n"
+  context "output redirection" do
+    before do
+      File.delete("tmp/output.txt") if File.exists?("tmp/output.txt")
+    end
+
+    it "should let you redirect stdout to a file" do
+      SafeShell.execute("echo", "Hello, world!", :stdout => "tmp/output.txt")
+      File.exists?("tmp/output.txt").should be_true
+      File.read("tmp/output.txt").should == "Hello, world!\n"
+    end
+
+    it "should let you redirect stderr to a file" do
+      SafeShell.execute("cat", "tmp/nonexistent-file", :stderr => "tmp/output.txt")
+      File.exists?("tmp/output.txt").should be_true
+      File.read("tmp/output.txt").should == "cat: tmp/nonexistent-file: No such file or directory\n"
+    end
   end
 
 end
